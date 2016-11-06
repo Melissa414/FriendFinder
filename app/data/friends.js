0d182604c97b2ca2c59032744ea9602488f932e5
Melissa414 <Missy41491@gmail.com>
2016-11-06 18:06:10 -0500

    just add some tweaks

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,98 +1,81 @@
-{
+var friends = [{
   "name":"Natsu Dragneel",
   "photo":"http://gph.is/22YJnEc",
   "scores":[
-     8,
-     10,
-     8,
-     6,
-     7,
-     7,
-     6,
-     1
+     "5",
+     "4",
+     "4",
+     "4",
+     "2",
+     "3",
+     "5",
+     "1"
    ]
-}
+},
 {
   "name":"Kakashi Hatake",
   "photo":"http://gph.is/1sCN1qG",
   "scores":[
-     6,
-     5,
-     3,
-     9,
-     7,
-     9,
-     10,
-     2
+     "5",
+     "4",
+     "5",
+     "5",
+     "2"
    ]
-}
+},
 {
   "name":"Light 'Kira' Yagami",
   "photo":"http://gph.is/1o0Ti67",
   "scores":[
-     8,
-     8,
-     9,
-     10,
-     6,
-     9,
-     5,
-     10
+     "5",
+     "4",
+     "5",
+     "3",
+     "5"
    ]
-}
+},
 {
   "name":"Monkey D. Luffy",
   "photo":"http://gph.is/2cGOt3q",
   "scores":[
-     10,
-     10,
-     8,
-     1,
-     6,
-     9,
-     2,
-     1
+     "3",
+     "4",
+     "5",
+     "2",
+     "1"
    ]
-}
+},
 {
   "name":"Goku",
   "photo":"http://gph.is/2cOeWRz",
   "scores":[
-     6,
-     8,
-     9,
-     4,
-     3,
-     7,
-     10,
-     2
+     "2",
+     "3",
+     "4",
+     "5",
+     "2"
    ]
-}
+},
 {
   "name":"Spike Spiegel",
   "photo":"http://gph.is/XL46wR",
   "scores":[
-     10,
-     3,
-     2,
-     4,
-     1,
-     1,
-     7,
-     3
+     "4",
+     "1",
+     "1",
+     "4",
+     "3"
    ]
-}
+},
 {
   "name":"Beerus",
   "photo":"http://gph.is/2cDeSUs",
   "scores":[
-     6,
-     2,
-     5,
-     4,
-     6,
-     1,
-     10,
-     10
+     "4",
+     "3",
+     "1",
+     "5",
+     "5"
    ]
-}
+}];
+module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,5 +1,6 @@
 var htmlRoutes = require('../routing/html-routes.js');
 var apiRoutes = require('../routing/api-routes.js');
+var friendsArray = require('../data/friends.js');
 
 
 module.exports = function (app) {
@@ -21,5 +22,66 @@ app.post('/api/html', function (req, res) {
       apiRoutes.push(req.body);
       res.json(false); // KEY LINE
     }
+
+var match = {};
+    var diffArray = [];
+    var totalNew = 0;
+    var count = 0;
+    var index = 0;
+    var total = 0;
+    var totalArray = [];
+    var differenceTotal = 0;
+    var friends = friendsArray;
+
+
+
+function totalNewArray(){
+      for (var i = 0; i<req.body.scores.length; i++){
+      totalNew += parseInt(req.body.scores[i]);
+      }
+
+      console.log(totalNew);
+      pushArray();
+    }
+function pushArray() {
+      for (var i = 0; i<friendsArray.length; i++) {
+        for (var x = 0; x<friendsArray[i].scores.length; x++) {
+          total += parseInt(friendsArray[i].scores[x]);
+        }
+        totalArray.push(total);
+        total = 0;
+      }
+      console.log(totalArray);
+      difference();
+    }
+function difference(){
+      for (var j = 0; j<totalArray.length; j++) {
+        differenceTotal = Math.abs(parseInt(totalNew) - parseInt(totalArray[j]));
+        diffArray.push(differenceTotal);
+        differenceTotal = 0;
+      }
+      console.log("This is the diffarray " + diffArray);
+      indexOfSmallest();
+    }
+function indexOfSmallest() {
+      index = 0;
+      var value = diffArray[0];
+      for (var i = 1; i < diffArray.length; i++) {
+        if (diffArray[i] < value) {
+            value = diffArray[i];
+            index = i;
+          }
+      }
+      matchSelect();
+    }
+function matchSelect(req) {
+      match = {
+        name: friendsArray[index].name,
+        photo: friendsArray[index].photo
+      };
+    }
+    console.log("Match found: " + match.name + "\n");
+    friendsArray.push(req.body);
+    res.json(match);
   });
 };
