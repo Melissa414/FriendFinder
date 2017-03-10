fc39b5f343558b3af27c75f3023f5ce7a3bd13ce
Melissa414 <Missy41491@gmail.com>
2017-03-10 16:00:34 -0500

    few tweaks

--- server.js
+++ server.js
@@ -1,43 +1,21 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-
-var app = express(); // Tells node that we are creating an "express" server
-var PORT = process.env.PORT || 8080; // Sets an initial port. We'll use this later in our listener
-
-// BodyParser makes it easy for our server to interpret data sent to it.
-// The code below is pretty standard.
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-
+//ROUTER
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
 
-app.get('/api/:characters?', function (req, res) {
-  var chosen = req.params.characters;
-
-  if (chosen) {
-    console.log(chosen);
-
-    for (var i = 0; i < characters.length; i++) {
-      if (chosen === characters[i].routeName) {
-        res.json(characters[i]);
-        return;
-      }
-    }
-
-    res.json(false);
-  } else {
-    res.json(characters);
-  }
-});
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
 });
